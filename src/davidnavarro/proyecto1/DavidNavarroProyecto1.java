aa0a978bca404d93e40eac03b957f54ea09fff95
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2024-09-26 19:17:59 -0600

    Primera subida Java p1
    Primera subida del proyecto 1 en Java

---
+++ src/davidnavarro/proyecto1/DavidNavarroProyecto1.java
@@ -0,0 +1,11 @@
+package davidnavarro.proyecto1;
+
+public class DavidNavarroProyecto1 {
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
---
+++ src/davidnavarro/proyecto1/Orden.java
@@ -0,0 +1,84 @@
+package davidnavarro.proyecto1;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+//Atributos de la clase Orden
+public class Orden {
+    private int numOrden;
+    private Date fechaOrden;
+    private ArrayList<String> detalleOrden = new ArrayList();
+    private String nombreCliente;
+    private double descuento;
+    private double total;
+
+    //Constructor sin parámetros
+    public Orden() {
+        
+        this.numOrden = 0;
+        this.fechaOrden = null;
+        this.nombreCliente = "";
+        this.descuento = 0.0;
+        this.total = 0.0;
+    }
+
+    public Orden(int numOrden, Date fechaOrden, String nombreCliente, double descuento, double total) {
+        this.numOrden = numOrden;
+        this.fechaOrden = fechaOrden;
+        this.nombreCliente = nombreCliente;
+        this.descuento = descuento;
+        this.total = total;
+    }
+
+    public int getNumOrden() {
+        return numOrden;
+    }
+
+    public void setNumOrden(int numOrden) {
+        this.numOrden = numOrden;
+    }
+
+    public Date getFechaOrden() {
+        return fechaOrden;
+    }
+
+    public void setFechaOrden(Date fechaOrden) {
+        this.fechaOrden = fechaOrden;
+    }
+
+    public ArrayList<String> getDetalleOrden() {
+        return detalleOrden;
+    }
+
+    public void setDetalleOrden(ArrayList<String> detalleOrden) {
+        this.detalleOrden = detalleOrden;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+    
+    
+   
+}
+
