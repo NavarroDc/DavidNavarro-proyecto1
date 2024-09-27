9d940e711f95ac0dd46255c8e172c7ff4824f4d5
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2024-09-27 16:28:43 -0600

    Clase producto hecha

--- src/davidnavarro/proyecto1/Producto.java
+++ src/davidnavarro/proyecto1/Producto.java
@@ -2,4 +2,35 @@ package davidnavarro.proyecto1;
 
 public class Producto {
     
+    private String nombreProducto;
+    private double precioProducto;
+
+    public Producto() {
+       this.nombreProducto = "";
+       this.precioProducto = 0.0;
+    }
+
+    public Producto(String nombreProducto, double precioProducto) {
+        this.nombreProducto = nombreProducto;
+        this.precioProducto = precioProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public double getPrecioProducto() {
+        return precioProducto;
+    }
+
+    public void setPrecioProducto(double precioProducto) {
+        this.precioProducto = precioProducto;
+    }
+    
+    
+    
 }
