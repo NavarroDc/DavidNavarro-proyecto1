b83e13ce4b440141c545c3346d70f6f47cc7d4e4
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2024-09-30 14:41:02 -0600

    se pasó el método menuPrincipal a la clase main

--- src/davidnavarro/proyecto1/DavidNavarroProyecto1.java
+++ src/davidnavarro/proyecto1/DavidNavarroProyecto1.java
@@ -10,6 +10,71 @@ public class DavidNavarroProyecto1 {
     public static void main(String[] args) {
         Orden objOrden = new Orden();
    
-        objOrden.menuPrincipal();
+        menuPrincipal();///acá la llamo
         }
+    //supongamos que aquí hago toda la funcionalidad del la función
+    public static void menuPrincipal(){
+        Scanner seleccion = new Scanner(System.in);
+        boolean opcionCorrecta = false;
+        int seleccionMenu = 0;
+
+        do {
+            try {
+                System.out.println("Opciones del restaurante, elija una opción:");
+                System.out.println("");
+                System.out.println("1. Creación de productos");
+                System.out.println("2. Toma de órdenes");
+                System.out.println("3. Ingreso de productos y precios ");
+                System.out.println("4. Reporte final");
+
+                seleccionMenu = Integer.parseInt(seleccion.nextLine());
+
+                if (seleccionMenu >= 1 && seleccionMenu <= 4) {
+
+                    opcionCorrecta = true;
+                } else {
+                    System.out.println("Debe ingresar un número del 1 al 4");
+                    opcionCorrecta = false;
+                }
+            } catch (NumberFormatException e){
+                System.out.println("Debe introducir un número en el apartado del menú");
+            }
+            
+            switch(seleccionMenu){
+                
+                case 1: 
+                    Orden nuevaOrden = new Orden();
+                    Scanner leerDatosProducto = new Scanner (System.in);
+                    boolean precioCorrecto = false;
+                    
+                    System.out.println("Ingrese el nombre del producto para crearlo");
+                    String nuevoNombreProducto = leerDatosProducto.nextLine();
+                    System.out.println("");
+                    
+                    do{
+                    try{
+                    System.out.println("Ingrese el precio del producto");
+                    String precioIngresado = leerDatosProducto.nextLine();
+                    Double nuevoPrecioProducto = Double.parseDouble(precioIngresado);
+                    Producto nuevoProducto = new Producto(nuevoNombreProducto, nuevoPrecioProducto);
+                    nuevaOrden.agregarProducto (nuevoProducto);
+                    precioCorrecto = true;
+                    }catch(NumberFormatException e){
+                        System.out.println("Debe ingresar un número para que el precio sea válido...");
+                    }
+                    }while(precioCorrecto == false);
+                break;
+                
+                case 2: System.out.println("Op2");
+                break;
+                
+                case 3: System.out.println("Op3");
+                break;
+                
+                case 4: System.out.println("Op4");
+                break;
+            }
+            }while (opcionCorrecta == false);
+            seleccion.close();
+    }
     }
--- src/davidnavarro/proyecto1/Orden.java
+++ src/davidnavarro/proyecto1/Orden.java
@@ -87,75 +87,7 @@ public class Orden {
         return "Orden{" + "numOrden=" + numOrden + ", fechaOrden=" + fechaOrden + ", detalleOrden=" + detalleOrden + ", nombreCliente=" + nombreCliente + ", descuento=" + descuento + ", total=" + total + '}';
     }
     //------------------------------------------------------------------------------------------------------------------------------------------
-    
-    public void menuPrincipal(){
-        Scanner seleccion = new Scanner(System.in);
-        boolean opcionCorrecta = false;
-        int seleccionMenu = 0;
 
-        do {
-            try {
-                System.out.println("Opciones del restaurante, elija una opción:");
-                System.out.println("");
-                System.out.println("1. Creación de productos");
-                System.out.println("2. Toma de órdenes");
-                System.out.println("3. Ingreso de productos y precios ");
-                System.out.println("4. Reporte final");
-
-                seleccionMenu = Integer.parseInt(seleccion.nextLine());
-
-                if (seleccionMenu >= 1 && seleccionMenu <= 4) {
-
-                    opcionCorrecta = true;
-                } else {
-                    System.out.println("Debe ingresar un número del 1 al 4");
-                    opcionCorrecta = false;
-                   
-                }
-
-            } catch (NumberFormatException e){
-                System.out.println("Debe introducir un número en el apartado del menú");
-                
-            }
-            
-            switch(seleccionMenu){
-                
-                case 1: 
-                    Orden nuevaOrden = new Orden();
-                    Scanner leerDatosProducto = new Scanner (System.in);
-                    boolean precioCorrecto = false;
-                    
-                    System.out.println("Ingrese el nombre del producto para crearlo");
-                    String nuevoNombreProducto = leerDatosProducto.nextLine();
-                    System.out.println("");
-                    
-                    do{
-                    try{
-                    System.out.println("Ingrese el precio del producto");
-                    String precioIngresado = leerDatosProducto.nextLine();
-                    Double nuevoPrecioProducto = Double.parseDouble(precioIngresado);
-                    Producto nuevoProducto = new Producto(nuevoNombreProducto, nuevoPrecioProducto);
-                    nuevaOrden.agregarProducto (nuevoProducto);
-                    precioCorrecto = true;
-                    }catch(NumberFormatException e){
-                        System.out.println("Debe ingresar un número para que el precio sea válido...");
-                    }
-                    }while(precioCorrecto == false);
-                break;
-                
-                case 2: System.out.println("Op2");
-                break;
-                
-                case 3: System.out.println("Op3");
-                break;
-                
-                case 4: System.out.println("Op4");
-                break;
-            }
-            }while (opcionCorrecta == false);
-            seleccion.close();
-    }
-    
     public void agregarProducto(Producto productoOrden) {
         this.detalleOrden.add(productoOrden);
     }
@@ -195,6 +127,7 @@ public class Orden {
         return totalFinal;
     }
 
+    //Mostrar datos debe ir en el main
     public void imprimirFactura() {
         for (Producto verProducto : this.detalleOrden) {
 
